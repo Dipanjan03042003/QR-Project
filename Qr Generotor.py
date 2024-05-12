3cafb9641b2a47d9008eeaf725fedd2971a230b6
Dipanjan03042003 <dipanjanghosh969@gmail.com>
2024-05-12 23:14:06 +0530

    QR project

---
+++ Qr Generotor.py
@@ -0,0 +1,3 @@
+import qrcode as qr
+img=qr.make("https://hips.hearstapps.com/hmg-prod/images/screen-shot-2022-12-12-at-3-48-26-pm-1670878181.png?crop=0.7797927461139896xw:1xh;center,top&resize=980:*")
+img.save("love.png")
